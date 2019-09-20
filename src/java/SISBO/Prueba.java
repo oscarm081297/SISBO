b5c0b1de77dd7aea1b20adb8b68d9e5bc2976d1c
oscarm081297 <50888671+oscarm081297@users.noreply.github.com>
2019-09-20 00:07:04 -0600

    Prueba WebService

---
+++ src/java/SISBO/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SISBO;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author oscar
+ */
+@Path("catArticulos")
+public class Prueba {
+    
+    
+}
